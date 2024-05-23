d5f28e2e5e0bf713843e672bc7d60ab18df948dc
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-05-23 11:49:40 +0200

    Update CSS and TypeScript files for better UI display
    A significant cleanup has been performed in the CSS file to reduce the complexity and focus on the primary UI elements. Additionally, important modifications have been made in the TypeScript file to enhance the application's function. These changes include adding table structure to the end of the file for better data display.

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
-const numbers: number[] = [1, 5, 17, 46, 523, 1361, 34461, 2473247347, 236283675287365];
+import "./style.css"
 
+const numbers: number[] = [1, 5, 17, 46, 523, 1361, 34461, 2473247347, 236283675287365];
+// @ts-ignore
 const doubledNumbers = numbers.map(number => number * 2);
 
 const words: string[] = ["Saubstauger", "Getreideförderband", "Elektrolyte", "Energieeinsparpotential"];
-
+// @ts-ignore
 const filteredWords = words.filter(word => word.length > 5);
 
 const moreNumbers: number[] = [17, 21, 1, 14, 5];
-
+// @ts-ignore
 const reducedNumbers = moreNumbers.reduce((a, b) => a + b);
 
-const evenMoreNumbers: number[] = [29,22136,125,4,-11];
-
+const evenMoreNumbers: number[] = [29, 22136, 125, 4, -11];
+// @ts-ignore
 const isEvenMoreNumbersGreaterThan10 = evenMoreNumbers.some(number => number > 10)
 
 
@@ -31,3 +33,23 @@ const newList2 = newList
 console.log(list)
 console.log(newList)
 console.log(newList2)
+
+
+const app = document.querySelector("#app")
+console.log(app)
+// @ts-ignore
+app.innerHTML = `
+    <table>
+        <tbody>
+            <tr>
+                <td>List:</td>
+                <td>${list.join(", ")}</td>
+            </tr>
+            <tr>
+                <td>Result:</td>
+                <td>${newList2}</td>
+            </tr>
+        </tbody>
+
+    </table>
+`;
