5a758a833e6957f0329843899dcc2220f94d6fa7
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-05-23 11:25:29 +0200

    Add bonus logic to main.ts
    The update introduces bonus logic to the main.ts file. It creates two new ordered lists and performs different operations on them – squaring each element and then filtering and summing the results. The results are then logged to the console.

--- src/main.ts
+++ src/main.ts
@@ -14,3 +14,20 @@ const evenMoreNumbers: number[] = [29,22136,125,4,-11];
 
 const isEvenMoreNumbersGreaterThan10 = evenMoreNumbers.some(number => number > 10)
 
+
+// Bonus
+
+const list: number[] = [15, 6, 3213, 9, 0, 12, 8464, 1, 1264, 481, 186, 1031, 194];
+
+const newList = [...list]
+    .sort((a, b) => b - a)
+    .slice(4, -2)
+    .map(number => number ** 2);
+
+const newList2 = newList
+    .filter(number => number % 4 !== 0)
+    .reduce((a, b) => a + b, 0);
+
+console.log(list)
+console.log(newList)
+console.log(newList2)
