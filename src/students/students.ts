6a87b41c5a9e508447d4c62a76d3440f84709fee
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-05-24 12:12:17 +0200

    Refactor student grades to include subjects
    A new structure is added to the student type where each grade is associated with a specific subject. This update enhances the readability of data by providing relevant context to the grades. The printGrades function is also adjusted to work with this new structure.

--- src/students/students.ts
+++ src/students/students.ts
@@ -1,52 +1,89 @@
 type validGermanGrades = 1 | 2 | 3 | 4 | 5 | 6 | undefined;
 type validAmericanGrades = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | undefined;
+type subject = {
+    subject: string,
+    grades: validGermanGrades[] | validAmericanGrades[]
+}
 
 type student = {
     firstName: string,
     lastName: string,
     age: number,
-    grades: validGermanGrades[] | validAmericanGrades[],
+    grades: subject[],
 };
 
 function printGrades(student: student) {
-    const head:string = student.firstName + " " + student.lastName + " (" + student.age + ")";
+    const head: string = student.firstName + " " + student.lastName + " (" + student.age + ")";
     console.log(head);
     console.log(getSeparator(head))
-    const grades = student.grades.map((grade) => {
-            if (grade === undefined) {
-                return '*'
-            } else {
-                return grade
-            }
+    console.log("Grades:")
+    student.grades.forEach((grade) => {
+            console.log(grade.subject + ": " + grade.grades.map((grade) => getGrade(grade)));
+
         }
     )
-    console.log("Grades: " + grades);
     console.log("");
-};
+}
+
+function getGrade(grade: validAmericanGrades | validGermanGrades) {
+    if (grade === undefined) {
+        return '*';
+    }
+    return grade;
+}
 
 
 const aljoscha: student = {
-firstName: "Aljoscha",
+    firstName: "Aljoscha",
     lastName: "Zöller",
     age: 17,
-    grades: [1, undefined, 'C', 6, 4, 'B',],
+    grades: [
+        {
+            subject: "Computer Science",
+            grades: [1, undefined, 'C', 6, 4, 'B',],
+        }
+    ],
 }
+
 type students = student[]
 
 const detlev: student = {
     firstName: "Detlev",
     lastName: "Arnolds",
     age: 87,
-    grades: [1, 'A', 2, 'B', 3, 'C',]
+    grades: [
+        {
+            subject: "Computer Science",
+            grades: [1, 'A', 2, 'B', 3, 'C',],
+        }
+    ],
 }
 
-const dieter: student = {...detlev, firstName: "Dieter", grades: [5, 'E', 6, 'F',]}
+const dieter: student = {
+    ...detlev,
+    firstName: "Dieter",
+    grades: [
+        {
+            subject: "Math",
+            grades: [5, 'E', 6, 'F',],
+        }
+    ]
+}
 
 const susie: student = {
     firstName: "Susie",
     lastName: "S.",
     age: 15,
-    grades: ['A', 'B', 'C']
+    grades: [
+        {
+            subject: "Naturschutz",
+            grades: ['A', 'B', 'C'],
+        },
+        {
+            subject: "Biologie",
+            grades: [1, 2, 3],
+        },
+    ],
 }
 
 const highSchoolStudents = [aljoscha, detlev, dieter, susie]
@@ -60,9 +97,9 @@ printAllStudents(highSchoolStudents)
 
 
 // Bonus
-function getSeparator(string:string) {
+function getSeparator(string: string) {
     string.length
-    let separator:string = "";
+    let separator: string = "";
 
     for (let i = 0; i < string.length; i++) {
         separator = separator + "=";
